a5d478454f220cc8b8006b52235af2321070cba9
ODAJIM17 <Davila56jd@gmail.com>
2024-08-18 09:37:30 -0400

    add countries entities

---
+++ Fantasy/Fantasy.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fantasy.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Country")]
+        [MaxLength(100, ErrorMessage = "{0} allows max {1} characters")]
+        [Required(ErrorMessage = "{0} is required.")]
+        public string Name { get; set; } = null!;
+    }
+}
