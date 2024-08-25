c932845a3c4c4f169e62277be40d2eba61f33572
ODAJIM17 <Davila56jd@gmail.com>
2024-08-25 16:43:55 -0400

    Add Create Country form

--- Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
+++ Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -1,11 +1,14 @@
 ﻿using Fantasy.Frontend.Repositories;
+using Fantasy.Frontend.Shared.Resources;
 using Fantasy.Shared.Entities;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 
 namespace Fantasy.Frontend.Pages.Countries
 {
     public partial class CountriesIndex
     {
+        [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
         [Inject] private IRepository Repository { get; set; } = null!;
 
         private List<Country>? Countries { get; set; }
